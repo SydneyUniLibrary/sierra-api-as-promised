80a521a5ecbe4bb1ba22b712cdbbed00769d5306
Jim Nicholls <jim.nicholls@sydney.edu.au>
2017-08-07 10:30:38 +1000

    Reformat code according to code style.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-"use strict"
+'use strict'
 
 /*
-  Copyright (C) 2017  The University of Sydney Library
+ Copyright (C) 2017  The University of Sydney Library
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 require('dotenv').load()
@@ -23,5 +23,5 @@ require('dotenv').load()
 
 module.exports = {
   about: require('./about'),
-  v4: require('./v4')
+  v4: require('./v4'),
 }
