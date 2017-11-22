9377bb3b332a2fc1fb7993dd8cc2f31e73ca6959
Jim Nicholls <jim.nicholls@sydney.edu.au>
2017-11-22 21:24:51 +1100

    Implement the toDateRange utility function.

--- test/dt-utils.js
+++ test/dt-utils.js
@@ -24,7 +24,7 @@ const { Interval } = require('luxon')
 
 const { chaiProperty, arbitrary } = require('../lib/test-support')
 
-const { toDateTimeRange } = require('../lib/dt-utils')
+const { toDateRange, toDateTimeRange } = require('../lib/dt-utils')
 
 
 
@@ -196,5 +196,171 @@ describe('dt-utils', function () {
 
   })
 
+
+  describe('toDateRange', function () {
+
+    it('returns undefined if given undefined, null or empty strings', function () {
+      expect(toDateRange(undefined)).to.be.undefined
+      expect(toDateRange(null)).to.be.undefined
+      expect(toDateRange('')).to.be.undefined
+      expect(toDateRange([])).to.be.undefined
+      expect(toDateRange([undefined, undefined])).to.be.undefined
+      expect(toDateRange([null, null])).to.be.undefined
+      expect(toDateRange(['', ''])).to.be.undefined
+    })
+
+    chaiProperty(
+      'passes through arbitrary non-empty strings',
+      jsv.nestring,
+      ( arg ) => {
+        expect(toDateRange(arg)).to.equal(arg)
+      }
+    )
+
+    chaiProperty(
+      'formats a single arbitrary date as an ISO date string',
+      arbitrary.date,
+      ( arg ) => {
+        expect(toDateRange(arg)).to.equal(arg.toISOString().slice(0, 10))
+      }
+    )
+
+    chaiProperty(
+      'formats a single arbitrary Luxon DateTime as an ISO date/time string',
+      arbitrary.dateTime,
+      ( arg ) => {
+        expect(toDateRange(arg)).to.equal(arg.toISO().slice(0, 10))
+      }
+    )
+
+    chaiProperty(
+      'formats a single arbitrary moment.js moment as an ISO date/time string',
+      arbitrary.moment,
+      ( arg ) => {
+        expect(toDateRange(arg)).to.equal(arg.toISOString().slice(0, 10))
+      }
+    )
+
+    chaiProperty(
+      'creates a closed range from two arbitrary ISO strings',
+      arbitrary.isoDateString,
+      arbitrary.isoDateString,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from},${to}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a left-open range from an arbitrary ISO string',
+      arbitrary.undefinedOrNull,
+      arbitrary.isoDateString,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[,${to}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a right-open range from an arbitrary ISO string',
+      arbitrary.isoDateString,
+      arbitrary.undefinedOrNull,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from},]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a closed range from two arbitrary dates',
+      arbitrary.date,
+      arbitrary.date,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from.toISOString().slice(0, 10)},${to.toISOString().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a left-open range from an arbitrary date',
+      arbitrary.undefinedOrNull,
+      arbitrary.date,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[,${to.toISOString().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a right-open range from an arbitrary date',
+      arbitrary.date,
+      arbitrary.undefinedOrNull,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from.toISOString().slice(0, 10)},]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a closed range from two arbitrary Luxon DateTime objects',
+      arbitrary.dateTime,
+      arbitrary.dateTime,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from.toISO().slice(0, 10)},${to.toISO().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a left-open range from an arbitrary Luxon DateTime',
+      arbitrary.undefinedOrNull,
+      arbitrary.dateTime,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[,${to.toISO().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a right-open range from an arbitrary Luxon DateTime',
+      arbitrary.dateTime,
+      arbitrary.undefinedOrNull,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from.toISO().slice(0, 10)},]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a closed range from an arbitrary Luxon Interval object',
+      arbitrary.dateTime,
+      arbitrary.period,
+      ( from, period ) => {
+        const inclusiveTo = from.plus(period)
+        const exclusiveTo = inclusiveTo.plus({ day: 1})
+        const interval = Interval.fromDateTimes(from, exclusiveTo)
+        expect(toDateRange(interval)).to.equal(`[${from.toISO().slice(0, 10)},${inclusiveTo.toISO().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a closed range from two arbitrary moment.js moment objects',
+      arbitrary.moment,
+      arbitrary.moment,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from.toISOString().slice(0, 10)},${to.toISOString().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a left-open range from an arbitrary moment.js moment object',
+      arbitrary.undefinedOrNull,
+      arbitrary.moment,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[,${to.toISOString().slice(0, 10)}]`)
+      }
+    )
+
+    chaiProperty(
+      'creates a right-open range from an arbitrary moment.js moment object',
+      arbitrary.moment,
+      arbitrary.undefinedOrNull,
+      ( from, to ) => {
+        expect(toDateRange([from, to])).to.equal(`[${from.toISOString().slice(0, 10)},]`)
+      }
+    )
+
+  })
 
 }) // describe('dt-utils'
